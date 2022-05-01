306a3e4094bbae9e8d45b825a1625b007e6aa8a9
harshach23 <>
2022-05-01 19:52:00 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { ethers } from "ethers";
+import './App.css';
+
+export default function App() {
+
+  const wave = () => {
+    
+  }
+  
+  return (
+    <div className="mainContainer">
+
+      <div className="dataContainer">
+        <div className="header">
+        👋 Hey there! Welcome
+        </div>
+
+        <div className="bio">
+        I am Harsha and I am currently pursuing my masters in Computer Science at USC. Connect your Ethereum wallet and wave at me!
+        </div>
+
+        <button className="waveButton" onClick={wave}>
+          Wave at Me
+        </button>
+      </div>
+    </div>
+  );
+}
