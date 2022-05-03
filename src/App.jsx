03554cde9d046405049d871e002a6ed82df82c32
Harsha Vardhan <36564415+harsha-vardhan-ch@users.noreply.github.com>
2022-05-03 05:03:22 +0000

    Called smart contract from web app

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,30 @@ import React from "react";
 import { useEffect, useState } from "react";
 import { ethers } from "ethers";
 import './App.css';
+import abi from "./utils/WavePortal.json";
 
 const App = () => {
-
+  /*
+  * Just a state variable we use to store our user's public wallet.
+  */
   const [ currentAccount, setCurrentAccount ] = useState("");
+   
+  /*
+    Create a variable here that holds the contract address after you deploy!
+   */
+  const contractAddress = "0x40D024EEA2723c3E272CfE0bf4fc35153D9081Dc";
 
+  /**
+   * Create a variable here that references the abi content!
+   */
+  const contractABI = abi.abi;
+  const [ waveCount, setWaveCount ] = useState(0);
+  
   const checkIfWalletIsConnected = async()=>{
     try {
+      /*
+    * First make sure we have access to window.ethereum
+    */
       const { ethereum } = window;
       if (!ethereum) {
         console.log("Make sure you have metamask!");
@@ -17,6 +34,9 @@ const App = () => {
         console.log("We have the ethereum object", ethereum);
       }
 
+      /*
+      * Check if we're authorized to access the user's wallet
+      */
       const { accounts } = await ethereum.request({method: "eth_accounts"});
 
       if (accounts.length !== 0) {
@@ -31,7 +51,10 @@ const App = () => {
       console.log(error);
     }
   }
-  
+
+  /*
+  * This runs our function when the page loads.
+  */
   useEffect (() => {
     checkIfWalletIsConnected();
   },[])
@@ -54,6 +77,62 @@ const App = () => {
     }
     
   }
+
+  const wave = async () => {
+    try {
+      const { ethereum } = window;
+
+
+      /* ethers is a library that helps our frontend talk to our contract
+      */
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+
+        /* Provider => talk to Ethereum nodes. 
+
+Remember how we were using Alchemy to deploy? 
+In this case we use nodes that Metamask provides in the background to send/receive data from our deployed contract.
+
+*/
+        const signer = provider.getSigner();
+
+        /*
+Signer => abstraction of an Ethereum Account.
+- To sign messages and transactions, send signed transactions to the Ethereum Network to execute state changing operations.
+*/
+
+        /* contractAddress => Address of the contract deployed , contractABI => File in Artifacts
+
+ABI file is something our web app needs to know how to communicate with our contract
+
+*/
+        const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer);
+
+        let count = await wavePortalContract.getTotalWaves();
+        console.log("Retrieved total wave count...", count.toNumber());
+
+         /*
+        * Execute the actual wave from your smart contract
+        */
+        const waveTxn = await wavePortalContract.wave();
+        console.log("Mining...", waveTxn.hash);
+
+        await waveTxn.wait();
+        console.log("Mined -- ", waveTxn.hash);
+
+        count = await wavePortalContract.getTotalWaves();
+        console.log("Retrieved total wave count...", count.toNumber());
+
+        setWaveCount(count);
+        
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+}
+  
   return (
     <div className="mainContainer">
 
@@ -66,11 +145,15 @@ const App = () => {
         I am Harsha and I am currently pursuing my masters in Computer Science at USC. Connect your Ethereum wallet and wave at me!
         </div>
 
-        <button className="waveButton" onClick={null}>
+        <button className="waveButton" onClick={wave}>
           Wave at Me
         </button>
 
 
+        <div className="user-wave-count">
+            Total number of users waved at Harsha : { waveCount }
+        </div>
+        
         {/*
         * If there is no currentAccount render this button
         */}
